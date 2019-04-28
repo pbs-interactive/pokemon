75edbd69d42e8449459e0c7bd76d3f3f4c48cbab
tanjamwil <50034499+tanjamwil@users.noreply.github.com>
2019-04-28 00:40:53 -0700

    Changed syntax because it was nasty

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,16 @@
 class pokemon:
 	def __init__(self, number, name, level, stats, moves):
-		self.number = number;#int 001
-		self.name = name;   #bulbasaur
-		self.level = level; #int 5
-		self.stats = stats; #array of ints? {hp/att/def/etc}
-		self.moves = moves; #array of type move
+		self.number = number#int 001
+		self.name = name   #bulbasaur
+		self.level = level #int 5
+		self.stats = stats #array of ints? {hp/att/def/etc}
+		self.moves = moves #array of type move
 
-	def print(self, pokemon): 	#print function
-		print("Pokedex #:" + number);
-		print("This pokemon is a level " + level + " " + name + ".");
+	def str(self): 	#print function
+		print "Pokedex #:" + self.number
+		print "This pokemon is a level " + self.level + " " + self.name + "."
+
+
+    def main(): #MAIN FUNCTION (for testing)
+        print()
+
