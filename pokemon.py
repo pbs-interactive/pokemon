35b1c202ede4841bdc7d1a6b7ea4bb17045f0de4
tanjamwil <50034499+tanjamwil@users.noreply.github.com>
2019-04-28 01:11:46 -0700

    Actually working edition
    removed main, as it's not needed in python lol
    
    fixed print function and made it so it prints the numbers in the format "001"
    
    added test

--- pokemon.py
+++ pokemon.py
@@ -1,16 +1,15 @@
 class pokemon:
-	def __init__(self, number, name, level, stats, moves):
-		self.number = number#int 001
-		self.name = name   #bulbasaur
-		self.level = level #int 5
-		self.stats = stats #array of ints? {hp/att/def/etc}
-		self.moves = moves #array of type move
+    def __init__(self, number, name, level, stats, moves):
+        self.number = number#int 001
+        self.name = name   #bulbasaur
+        self.level = level #int 5
+        self.stats = stats #array of ints? {hp/att/def/etc}
+        self.moves = moves #array of type move
 
-	def str(self): 	#print function
-		print "Pokedex #:" + self.number
-		print "This pokemon is a level " + self.level + " " + self.name + "."
+    def printMe(self): 	#print function
+        print("Pokedex#: " + str(self.number).zfill(3))
+        print ("This pokemon is a level " + str(self.level) + " " + self.name + ".")
 
-
-    def main(): #MAIN FUNCTION (for testing)
-        print()
-
+#Testing __init__ and printMe
+bulbasaur = pokemon(001, 'bulbasaur', 005, 'placeholder', 'placeholder')
+bulbasaur.printMe()
